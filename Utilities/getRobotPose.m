ff7b18f6b05a4bb5ff9e97c28d1c8f81040e8837
Sebastian Castro <sebas.a.castro@gmail.com>
2019-04-06 17:31:28 -0400

    Generalized getRobotPose variable naming

--- Utilities/getRobotPose.m
+++ Utilities/getRobotPose.m
@@ -1,15 +1,15 @@
-function pose = getRobotPose(odomMsg)
-% Helper function that extracts pose from odometry message
+function pose = getRobotPose(poseMsg)
+% Helper function that extracts 2D pose (x y theta) from ROS pose message
 
-% Copyright 2018 The MathWorks, Inc.
+% Copyright 2018-2019 The MathWorks, Inc.
 
     % Unwrap position
-    position = odomMsg.Pose.Pose.Position;
+    position = poseMsg.Pose.Pose.Position;
     x = position.X;
     y = position.Y;
 
     % Unwrap orientation
-    orientation = odomMsg.Pose.Pose.Orientation;
+    orientation = poseMsg.Pose.Pose.Orientation;
     q = [orientation.W, orientation.X, ...
          orientation.Y, orientation.Z];
     r = quat2eul(q);
