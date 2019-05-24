f953358ef1b16ff82e28d4fbd59f4ef01f5bd22a
scastro <scastro@ah-scastro.dhcp.mathworks.com>
2019-05-24 10:20:07 +0200

    Changed age detector to a better network. Added TTS example using ROS sound_play.

---
+++ Vision/person_detection/setupAgeDetector.m
@@ -0,0 +1,65 @@
+% Setup function for age detection network
+% Copyright 2019 The MathWorks, Inc.
+function setupAgeDetector()
+
+% Define folder/model names and options
+modelFolder = fullfile(fileparts(mfilename('fullpath')),'models');
+modelAge = 'ageNetwork';
+ageGenderMeanImage = 'age_gender_mean';
+insertIntoProtoFile = @(modelFolder,filename,DataType) cat(2, ...
+    'layer {', newline,'  top: "data"', newline, '  top: "label"', newline, ...
+    '  name: "data"', newline, '  type: ', DataType, newline, ... 
+    '  transform_param {', newline, '    crop_size: 224', newline, ...
+    '    mirror: false', newline, ...
+    '    mean_file: "', modelFolder, '/', filename, '.binaryproto"', newline, ...
+    '  }', newline, '  include: { ', newline, '    phase: TEST', newline, ...
+    '    stage: "test-on-test"', newline, ' }', newline, '}', newline);
+options = weboptions('ContentType','text');
+
+% Download Mean Image for age and gender classification
+if ~isfile(fullfile(modelFolder,[ageGenderMeanImage,'.binaryproto']))
+    fprintf(1,'Downloading mean image for age and gender classification...')
+
+    % Download binaryproto
+    ageGenderAuxiliaryFiles = websave(fullfile(modelFolder,'caffe_ilsvrc12.tar.gz'), ...
+        'http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz');
+    untar(ageGenderAuxiliaryFiles,fullfile(modelFolder,'auxiliaryFiles'))
+    copyfile(fullfile(modelFolder,'auxiliaryFiles','imagenet_mean.binaryproto'),...
+        fullfile(modelFolder,[ageGenderMeanImage,'.binaryproto']))
+    rmdir(fullfile(modelFolder,'auxiliaryFiles'),'s')
+    delete(ageGenderAuxiliaryFiles)
+
+    fprintf(1,'[DONE]\n')
+end
+
+% Download Caffe model and protofile for age classification
+if ~isfile(fullfile(modelFolder,[modelAge,'.caffemodel']))
+    fprintf(1,'Downloading Caffe model for age classification. This may take a few minutes...');
+    netAgeModelFilename = websave(fullfile(modelFolder,[modelAge,'.caffemodel']), ...
+        'https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/dex_chalearn_iccv2015.caffemodel');
+else
+    netAgeModelFilename = fullfile(modelFolder,[modelAge,'.caffemodel']);
+end
+
+% Download and modify age protofile to include mean image    
+netAgeProtoFilename = fullfile(modelFolder,[modelAge,'_deploy.prototxt']);
+if ~isfile(fullfile(modelFolder,[modelAge,'._deploy.prototxt']))
+    netAgeProtoFile = webread('https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/age.prototxt',options);
+    netAgeProtoFile = [netAgeProtoFile(1:29), insertIntoProtoFile(modelFolder,ageGenderMeanImage,'"ImageData"'), netAgeProtoFile(100:end)];
+    fid = fopen(netAgeProtoFilename,'wt');
+    fprintf(fid, '%s', netAgeProtoFile);
+    fclose(fid);
+end
+
+fprintf(1,'[DONE]\n')
+    
+% Prepare age classification network in MATLAB format
+if ~isfile(fullfile(modelFolder,[modelAge,'.mat']))
+    fprintf(1,'Converting age classification network to MATLAB format...');
+    classes = string(0:100);
+    ageNet = importCaffeNetwork(netAgeProtoFilename,netAgeModelFilename,'Classes',classes,'InputSize',[224,224,3]);  % Average image to be taken from protofile
+    save(fullfile(modelFolder,modelAge),'ageNet');
+    fprintf(1,'[DONE]\n')
+end
+
+end
