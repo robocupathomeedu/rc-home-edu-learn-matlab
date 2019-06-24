abf809ecbd837119bab133c9ec5cfd5f621326d5
Sebastian Castro <sebas.a.castro@gmail.com>
2019-06-24 10:57:41 -0400

    Added ROS Configuration App

--- Utilities/connectToRobot.m
+++ Utilities/connectToRobot.m
@@ -1,14 +1,21 @@
 % Robot Connection Script
-% Copyright 2018 The MathWorks, Inc.
+% Copyright 2018-2019 The MathWorks, Inc.
 
 %% Shut down connection to ROS, if any
 rosshutdown
 
 %% Connect to ROS master
-% Replace with your IP address information
-% Set to '' if MATLAB is on the same machine as ROS master
-robotIP = '';
-matlabIP = '';
+% Replace with your ROS configuration
+configName = 'myTopicConfig';
+load(configName);
+
+% Set environment variables
+if ~isempty(matlabIP)
+    setenv('ROS_IP',matlabIP);
+end
+if ~isempty(robotIP)
+    setenv('ROS_MASTER_URI',"http://" + robotIP + ":11311");
+end
 
 % Connect to ROS master
 rosinit(robotIP,'NodeHost',matlabIP)
@@ -21,46 +28,3 @@ rosinit(robotIP,'NodeHost',matlabIP)
 %r = rosdevice(robotIP,'turtlebot','turtlebot');
 %r.ROSFolder = '/opt/ros/kinetic';
 %r.CatkinWorkspace = '~/ros/catkin_ws';
-
-%% ROS Topics and frames
-ROBOT_CMD_VEL = '/cmd_vel'; 
-ROBOT_ODOM = '/odom';
-ROBOT_RESET_ODOM = '/reset_odometry';
-ROBOT_POSE = '/amcl_pose';
-LASER_SCAN = '/scan';
-RGB_IMAGE = '/camera/rgb/image_raw'; 
-DEPTH_IMAGE = '/camera/depth/image_raw'; 
-DEPTH_POINTS = '/camera/depth_registered/points';
-MAP_TOPIC = '/map';
-MAP_FRAME = '/map';
-JOINT_STATES = '/joint_states';
-MOVE_BASE_ACTION = '/move_base';
-
-
-%
-% !!! Set your platform here !!!
-%
-robottype = 'TurtleBot';
-
-% Platform specific names
-
-% TurtleBot
-switch lower(robottype) %  Use lowercase so it's case insensitive
-    case 'turtlebot'
-        ROBOT_CMD_VEL = '/mobile_base/commands/velocity'; 
-        ROBOT_ODOM = '/odom';
-        ROBOT_RESET_ODOM = '/mobile_base/commands/reset_odometry';
-        LASER_SCAN = '/scan';
-        RGB_IMAGE = '/camera/rgb/image_raw'; 
-        DEPTH_IMAGE = '/camera/depth_registered/image_raw'; 
-        DEPTH_POINTS = '/camera/depth_registered/points';
-
-% MARRtino
-    case 'marrtino'
-        RGB_IMAGE = '/rgb/image_raw'; 
-        DEPTH_IMAGE = '/depth/image_raw'; 
-
-% Stage simulator
-    case 'stage'
-
-end
